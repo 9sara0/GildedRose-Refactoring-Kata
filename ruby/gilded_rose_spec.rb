80c48fc5df77970e0f7cdb83fa663143d1084bf5
Sara <sarah.alalaya@gmail.com>
2016-11-30 16:08:11 +0300

    Item: updates 'aged_brie?' method spec test

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -15,8 +15,20 @@ end
 
 
 describe Item do
-  it "knows if its an 'Aged Brie'" do
-    item = Item.new(name="Aged Brie", sell_in=2, quality=0)
-    expect(item).to be_aged_brie
+  describe "#aged_brie?" do
+    it "knows if its an 'Aged Brie'" do
+      item = Item.new(name="Aged Brie", sell_in=2, quality=0)
+      expect(item).to be_aged_brie
+    end
+
+    it "knows if it is not an 'Aged Brie'" do
+      item = Item.new(name="Dexterity Vest", sell_in=2, quality=1)
+      expect(item).not_to be_aged_brie
+    end
+  end
+
+  it "knows if its a 'Backstage passes'" do
+    item = Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20)
+    expect(item).to be_backstage_passes
   end
 end
